3950c88d9f900c528c22bf530cbb653a72e431ac
Gabriel Rufino <>
2019-01-19 15:50:20 -0300

    route and component to login

--- src/router.js
+++ src/router.js
@@ -1,9 +1,15 @@
 import VueRouter from 'vue-router'
 
+import Login from '@/pages/Login'
 import Home from '@/pages/Home'
 import Targets from '@/pages/Targets'
 
 const routes = [
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
   {
     path: '/',
     name: 'home',
