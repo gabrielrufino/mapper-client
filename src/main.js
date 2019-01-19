c956a1833e472f793cde855cfdf22a306b45ab7e
Gabriel Rufino <>
2019-01-19 15:45:53 -0300

    Loading the vue resource in the vue instance

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 
 import "bulma/css/bulma.css"
 import "@fortawesome/fontawesome-free/css/all.css"
@@ -8,6 +9,7 @@ import router from './router'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
+Vue.use(VueResource)
 
 new Vue({
   render: h => h(App),
