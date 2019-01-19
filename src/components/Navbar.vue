de017e2028aa21e1969a596ea71c3180fa3f8c83
Gabriel Rufino <>
2019-01-18 22:56:11 -0300

    Initial navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+      </div>
+
+      <div class="navbar-menu">
+      </div>
+    </nav>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
