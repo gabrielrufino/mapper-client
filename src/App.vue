62d8dad546a1342a41c77f897579a2a778b1ed44
Gabriel Rufino <>
2019-01-18 23:07:03 -0300

    Importing Navbar in App

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div id="app">
+    <navbar/>
   </div>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    Navbar
+  }
 }
 </script>
