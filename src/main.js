721552b3d14cb78aade0d011c25ed67e59366a36
Gabriel Rufino <>
2019-01-19 09:49:27 -0300

    importing router

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import VueRouter from 'vue-router'
 import "bulma/css/bulma.css"
 import "@fortawesome/fontawesome-free/css/all.css"
 import App from './App.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
