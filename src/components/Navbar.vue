87d8fc098c5020e90430924c3dddee906f94c8a7
Gabriel Rufino <>
2019-01-18 23:22:48 -0300

    upgrade in the navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,28 @@
 <template>
   <div>
 
-    <nav class="navbar" role="navigation" aria-label="main navigation">
+    <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="#">
+            mapper
+          </a>
+
+          <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
       </div>
 
       <div class="navbar-menu">
+        <div class="navbar-start">
+          <a class="navbar-item" href="#">Home</a>
+        </div>
+        <div class="navbar-end">
+
+        </div>
       </div>
     </nav>
 
